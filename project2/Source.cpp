324d6c56dfb8328c7f078de205498ae7e8355881
piy-bebe <piy.bebe@gmail.com>
2021-11-01 15:40:41 +0300

    created project2

---
+++ project2/Source.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+
+	cout << "Привет мир :)";
+	cout << "Привет мир :)";
+	cout << "Привет мир :)";
+	cout << "Привет мир :)";
+	cout << "Привет мир :)";
+
+	return 0;
+}
